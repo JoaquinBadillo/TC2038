eed8accd83b3fc07f43c3c311ba578ed894cf7f5
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2023-11-16 09:22:28 -0600

    Dijkstra and Floyd 🏁

--- 05_Graphs/01_Shortest_Path/main.cpp
+++ 05_Graphs/01_Shortest_Path/main.cpp
@@ -2,7 +2,15 @@
 #include "lib/floyd.hpp"
 
 int main(int argc, char const *argv[]) {
-    sp::init("input.txt");
+    std::string filename = "input.txt";
+
+    if (argc > 1)
+        filename = argv[1];
+
+    sp::init(filename);
+
+    if (sp::nodes == 0)
+        return 0;
 
     std::cout << "Dijkstra:" << std::endl;
     for(int i = 0; i < sp::nodes; ++i)
